2dacea4dd2d7288bd927b070fbfe7e70d8ebfd40
ariel ortega <ariel.ortega.zitle98@gmail.com>
2022-01-16 18:13:57 -0600

    agregue sql hairsalon

---
+++ hairsalon.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 17-01-2022 a las 01:09:12
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `hairsalon`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios_login`
+--
+
+CREATE TABLE `usuarios_login` (
+  `usuario` varchar(30) NOT NULL,
+  `contrasena` varchar(30) NOT NULL,
+  `metodo` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios_login`
+--
+
+INSERT INTO `usuarios_login` (`usuario`, `contrasena`, `metodo`) VALUES
+('ariel', '689150106b3524a078a0ef39190456', 'sha1'),
+('ortega', 'f6f9ada5616798aa9e0ba80c7f1857', 'md5'),
+('zitle', 'ariel1304', 'texto');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usuarios_login`
+--
+ALTER TABLE `usuarios_login`
+  ADD PRIMARY KEY (`usuario`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
